c69fe16e108f363433f9ff459fe05889cc2c8ede
KambalaFish <justdoitnikey@gmail.com>
2022-10-01 14:37:55 +0700

    script modification

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,10 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Smart Enterprise</title>
-        <script src="{{asset('js/app.js')}}" defer></script>
         <style>
             @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
         </style>
+        <script src="/js/app.js"></script>
     </head>
     <body class="antialiased">
         <div id="root"></div>
